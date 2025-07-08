f365e1e5d3227c57f1a063b5c652986c0d582f8a
saja-ahmed <127935811+saja-ahmed@users.noreply.github.com>
2025-07-08 07:10:57 +0300

    Fix: ignore TypeScript any error for disable-devtools

--- src/utils/disable-devtools.ts
+++ src/utils/disable-devtools.ts
@@ -1,9 +1,6 @@
-// src/utils/disable-devtools.ts
-if (typeof window !== "undefined") {
-    (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ = {
-      supportsFiber: true,
-      inject: () => {},
-      onCommitFiberRoot: () => {},
-      onCommitFiberUnmount: () => {},
-    };
-  }
+(window as unknown as { [key: string]: unknown }).__REACT_DEVTOOLS_GLOBAL_HOOK__ = {
+  supportsFiber: true,
+  inject: () => {},
+  onCommitFiberRoot: () => {},
+  onCommitFiberUnmount: () => {},
+};
